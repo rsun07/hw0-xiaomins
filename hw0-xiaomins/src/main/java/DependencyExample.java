ab924c0f76c1e2f4fa35b05029cebb1d1e70b278
rsun07 <rsun2014@gmail.com>
2014-09-03 17:28:17 -0400

    update

---
+++ hw0-xiaomins/src/main/java/DependencyExample.java
@@ -0,0 +1,16 @@
+import java.io.StringReader;
+
+edu.stanford.nlp.ling.Word;
+edu.stanford.nlp.objectbank.TokenizerFactory;
+edu.stanford.nlp.process.PTBTokenizer.PTBTokenizerFactory;
+edu.stanford.nlp.process.Tokenizer;
+
+/**
+* An example for Homework 0 of 11791 F13
+*
+* @author Zi Yang <ziy@cs.cmu.edu>
+*/
+public class DependencyExample {
+/**
+* Tokenize a sentence in the argument, and print out
+* the tokens to the console.
